2a1a25a1d7d2d9472b6cdfdad4710f083fa36330
kukuyu-0524 <1907257031@qq.com>
2025-04-25 13:46:09 +0800

    second commit

--- server.c
+++ server.c
@@ -1,52 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <winsock2.h>
-
-#pragma comment(lib, "ws2_32.lib")
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define PORT 8888
 #define BUFFER_SIZE 1024
 
 int main() {
-    WSADATA wsaData;
-    SOCKET server_socket, client_socket;
+    int server_socket, client_socket;
     struct sockaddr_in server_addr, client_addr;
     char buffer[BUFFER_SIZE];
-    int client_addr_size = sizeof(client_addr);
-
-    // 初始化 Winsock
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        printf("WSAStartup 失败\n");
-        return 1;
-    }
+    socklen_t client_addr_size = sizeof(client_addr);
 
     // 创建套接字
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == INVALID_SOCKET) {
+    if (server_socket < 0) {
         printf("套接字创建失败\n");
-        WSACleanup();
         return 1;
     }
 
     // 设置服务器地址结构
+    memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
 
     // 绑定套接字
-    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
+    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         printf("绑定失败\n");
-        closesocket(server_socket);
-        WSACleanup();
+        close(server_socket);
         return 1;
     }
 
     // 监听连接
-    if (listen(server_socket, 5) == SOCKET_ERROR) {
+    if (listen(server_socket, 5) < 0) {
         printf("监听失败\n");
-        closesocket(server_socket);
-        WSACleanup();
+        close(server_socket);
         return 1;
     }
 
@@ -54,10 +47,9 @@ int main() {
 
     // 接受客户端连接
     client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_addr_size);
-    if (client_socket == INVALID_SOCKET) {
+    if (client_socket < 0) {
         printf("接受连接失败\n");
-        closesocket(server_socket);
-        WSACleanup();
+        close(server_socket);
         return 1;
     }
 
@@ -66,7 +58,8 @@ int main() {
     // 接收和发送数据
     while (1) {
         memset(buffer, 0, BUFFER_SIZE);
-        if (recv(client_socket, buffer, BUFFER_SIZE, 0) > 0) {
+        ssize_t bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0);
+        if (bytes_received > 0) {
             printf("收到消息: %s\n", buffer);
             
             // 发送回显消息
@@ -77,9 +70,8 @@ int main() {
     }
 
     // 清理
-    closesocket(client_socket);
-    closesocket(server_socket);
-    WSACleanup();
+    close(client_socket);
+    close(server_socket);
 
     return 0;
 }
